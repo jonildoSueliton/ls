fb7ed09e1a5206e7919efe6393317ff01542a675
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2017-01-18 00:36:20 -0300

    finalizando pallete-color, cepinfo, post

---
+++ classroom/w3c/dom/getCEPInfo.js
@@ -0,0 +1,12 @@
+// viacep.com.br/ws/01001000/json/
+const cep = {
+  cep: "01001-000",
+  logradouro: "Praça da Sé",
+  complemento: "lado ímpar",
+  bairro: "Sé",
+  localidade: "São Paulo",
+  uf: "SP",
+  unidade: "",
+  ibge: "3550308",
+  gia: "1004"
+};
